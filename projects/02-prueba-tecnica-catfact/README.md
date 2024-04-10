57d702fe65e9e341512b8a5188d9eda2b3e4d44b
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-04-10 09:40:42 -0300

    Cambio de API de imagenes de gatos, cataas no esta andando

--- projects/02-prueba-tecnica-catfact/README.md
+++ projects/02-prueba-tecnica-catfact/README.md
@@ -4,6 +4,7 @@ APIs:
 
 - Facts Random: https://catfact.ninja/fact
 - Imagen random: https://cataas.com/cat/says/hello
+- !!! Estoy usando otra API por que cataas no esta funcionando, sin utilizar la palabra extraida del facto => https://api.thecatapi.com/v1/images/search
 
 - Recupera un hecho aleatorio de gatos de la primera API
 - Recuperar la primera palabra del hecho
--- projects/02-prueba-tecnica-catfact/src/App.jsx
+++ projects/02-prueba-tecnica-catfact/src/App.jsx
@@ -1,5 +1,47 @@
-function App() {
-  return <h1>CAT FATCS</h1>;
-}
+import { useState, useEffect } from "react";
 
-export default App;
+const URL_API_FACT = "https://catfact.ninja/fact";
+//const URL_API_IMG = "https://cataas.com/cat/says/";
+
+//Estoy usando otra API por que cataas no esta funcionando...
+const URL_API_IMG_ALT = "https://api.thecatapi.com/v1/images/search";
+
+export default function App() {
+  const [fact, setFact] = useState("");
+  const [firstWord, setFirstWord] = useState("");
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    fetch(URL_API_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const newFact = data.fact;
+        setFact(newFact);
+        setFirstWord(newFact.split(" ")[0]);
+      });
+  }, []);
+
+  useEffect(() => {
+    //const URL = URL_API_IMG + firstWord;
+    const URL = URL_API_IMG_ALT;
+
+    if (fact) {
+      fetch(URL)
+        .then((res) => res.json())
+        .then((data) => setImage(data[0].url));
+    }
+  }, [fact]);
+
+  return (
+    <main>
+      <h1>CAT FATCS</h1>
+      <p>
+        <u>Fact:</u> {fact}
+      </p>
+      <p>
+        <u>First word:</u> {firstWord}
+      </p>
+      {image && <img src={image} alt="Foto de un gato random" />}
+    </main>
+  );
+}
