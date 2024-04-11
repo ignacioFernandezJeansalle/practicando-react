72dc80dab3710bc31368c0319ce2b310053e5288
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-04-11 16:19:53 -0300

    Componetizacion App y estilos generales

--- projects/03-prueba-tecnica-buscador-peliculas/src/App.jsx
+++ projects/03-prueba-tecnica-buscador-peliculas/src/App.jsx
@@ -1,9 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+import Movies from "./components/Movies";
+import useMovies from "./hooks/useMovies";
 
 export default function App() {
+  const [searchParams, setSearchParams] = useState(null);
+  const { movies } = useMovies({ searchParams });
+
+  /* const handleChange = (event) => {
+    setSearchParams(event.target.value);
+  }; */
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Recuperar los parametros de búsqueda del input
+    const { search } = Object.fromEntries(new window.FormData(event.target));
+    setSearchParams(search);
+  };
+
   return (
-    <main>
-      <h1>Prueba técnica - Buscador de películas 🔎</h1>
-    </main>
+    <>
+      <header>
+        <h1>Prueba técnica - Buscador de películas 🔎</h1>
+        <form onSubmit={handleSubmit}>
+          <input name="search" type="text" placeholder="Superman, Batman, La vida es bella...." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+
+      <main>
+        <Movies movies={movies} />
+      </main>
+    </>
   );
 }
